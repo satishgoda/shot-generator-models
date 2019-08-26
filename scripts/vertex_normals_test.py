62ad8bae1d95a8e5cee0eb7a8768da67be99f6cc
Satish Goda <satishgoda@live.com>
2019-08-26 10:38:14 -0700

    Update vertex_normals_test.py

--- scripts/vertex_normals_test.py
+++ scripts/vertex_normals_test.py
@@ -1,17 +1,19 @@
 import bpy
 from mathutils import Vector
 
+
 aodata = bpy.context.active_object.data
 
+# https://docs.blender.org/api/master/bpy.types.MeshVertex.html
+# vtx.normal is the pre-calculated vertex normal
 vertex_normals = {index: ([], vtx.normal)  for index, vtx in enumerate(aodata.vertices)}
 
+# https://docs.blender.org/api/master/bpy.types.MeshPolygon.html
 for polygon in aodata.polygons:
     for vertex_index in polygon.vertices:
         face_normals = vertex_normals[vertex_index][0]
+        # We are going to sum the polygon normal vectors later and normalize to get vertex normal
         face_normals.append(polygon.normal)
-
-
-face_normals, pre_calculated_vertex_normal = vertex_normals[0]
 
 for face_normals, pre_calculated_vertex_normal in vertex_normals.values():
     vertex_normal = sum(face_normals, Vector((0.0, 0.0, 0.0)))
