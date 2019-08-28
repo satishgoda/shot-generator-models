adef3ea7e9620057b0b322999021b9a5537836ea
Satish Goda <satishgoda@live.com>
2019-08-27 23:35:25 -0700

    Update operator_vertex_normals_to_color.py

--- scripts/operator_vertex_normals_to_color.py
+++ scripts/operator_vertex_normals_to_color.py
@@ -1,8 +1,9 @@
 import bpy
+from mathutils import Color
 
 
 # https://gist.github.com/Kodagrux/5b39358d812c0fd8eaf4
-def remap(value, maxInput, minInput, maxOutput, minOutput):
+def remap_value(value, minInput, maxInput,  minOutput, maxOutput):
     value = maxInput if value > maxInput else value
     value = minInput if value < minInput else value
 
@@ -14,34 +15,48 @@ def remap(value, maxInput, minInput, maxOutput, minOutput):
     return minOutput + (scaledThrust * outputSpan)
 
 
+def get_color_for_normal(normal, remap=True):
+    color = Color(normal)
+
+    if remap:
+        color.r = remap_value(color.r, -1, 1, 0, 1)
+        color.g = remap_value(color.g, -1, 1, 0, 1)
+        color.b = remap_value(color.b, -1, 1, 0, 1)
+
+    return color
+
+
 class VertexNormalToVertexColorsOperator(bpy.types.Operator):
-    """Convert averaged vertex normals to vertex colors"""
+    """Convert vertex normals to vertex colors"""
     bl_idname = "object.bake_vertex_normal_to_color"
     bl_label = "Bake Vertex Normal To Color"
 
     @classmethod
     def poll(cls, context):
-        return (context.active_object is not None)
+        active_object = context.active_object
+        return (active_object is not None \
+                and active_object.type == 'MESH')  
 
     def execute(self, context):
-        aodata = context.active_object.data
+        mesh = context.active_object.data
 
-        if not 'vertex_normals' in aodata.vertex_colors:
-            aodata.vertex_colors.new(name='vertex_normals')
+        if not 'vertex_normals' in mesh.vertex_colors:
+            mesh.vertex_colors.new(name='vertex_normals')
 
-        color_layer = aodata.vertex_colors['vertex_normals']
+        color_layer = mesh.vertex_colors['vertex_normals']
         color_layer.active = True
         color_layer.active_render = True
 
         i = 0
-        for poly in aodata.polygons:
-            for idx in poly.vertices:
-                normal = aodata.vertices[idx].normal
+        for polygon in mesh.polygons:
+            for vertex_index in polygon.vertices:
+                normal = mesh.vertices[vertex_index].normal
         
-                r = remap(normal.x, 1, -1, 1, 0)
-                g = remap(normal.y, 1, -1, 1, 0)
-                b = remap(normal.z, 1, -1, 1, 0)
+                color = get_color_for_normal(normal, remap=True)
+                r, g, b = color
+                
                 color_layer.data[i].color = (r, g, b, 1.0)
+                
                 i += 1
         return {'FINISHED'}
 
